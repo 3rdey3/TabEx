aeebf186b822bc06387dadcbd7a9fa99b8032a71
Navid Forhad <navid.forhad@ucb.com.bd>
2021-09-28 12:12:20 +0600

    moving parsing logics to separate file

--- src/components/tabex-component.js
+++ src/components/tabex-component.js
@@ -1,45 +1,5 @@
-﻿(function(Vue, Fuse, window) {
-    async function getCurrentTab() {
-        let queryOptions = { active: true, currentWindow: true };
-        let [tab] = await chrome.tabs.query(queryOptions);
-        return tab;
-    }
-
-    async function getAllTabUrls() {
-        let tabs = await chrome.tabs.query({});
-        let urls = tabs.map(t => {return { url: new URL(t.url), tab: t }});
-        return urls;
-    }
-
-    function parseByHost(urls) {
-        let hosts = [];
-        urls.forEach(url => {
-            if (!hosts.some(host => host.name === url.url.host)) {
-                let host = {};
-                host.name = url.url.host;
-                if (!!url.tab.favIconUrl) {
-                    host.icon = url.tab.favIconUrl;
-                } else {
-                    host.icon = '../icons/tabex16x16.png';
-                }
-                host.children = urls.filter(curl => curl.url.host === host.name);
-                hosts.push(host);
-            }
-        });
-        return hosts.sort((a,b) => {
-            var nameA = a.name.toUpperCase(); // ignore upper and lowercase
-            var nameB = b.name.toUpperCase(); // ignore upper and lowercase
-            if (nameA < nameB) {
-                return -1;
-            }
-            if (nameA > nameB) {
-                return 1;
-            }
-
-            // names must be equal
-            return 0;
-        });
-    }
+﻿(function (Vue, Fuse, window) {
+    let parser = window.TabEx.parser;
 
     let TabEx = {
         data() {
@@ -50,15 +10,13 @@
         },
         mounted() {
             (async() => {
-                var urls = await getAllTabUrls();
-                this.urls = parseByHost(urls);
+                var urls = await parser.getAllTabUrls();
             })();
         },
         methods: {
             applyFilter: function (filterText) {
                 (async () => {
-                    let urls = await getAllTabUrls();
-                    let purls = parseByHost(urls);
+                    let urls = await parser.getAllTabUrls();
                     this.searchText = filterText;
                     if (!!filterText && filterText.length) {
                         const fuse = new Fuse(purls, {threshold:0.3, minMatchCharLength: 5, keys: [ "children.tab.title", "children.tab.url" ]});
---
+++ src/parser.js
@@ -0,0 +1,52 @@
+﻿(function (window, chrome) {
+    let parser = window.TabEx.parser || {};
+    let config = window.TabEx.config;
+
+    async function getCurrentTab() {
+        let queryOptions = { active: true, currentWindow: true };
+        let [tab] = await chrome.tabs.query(queryOptions);
+        return tab;
+    };
+    parser.getCurrentTab = getCurrentTab;
+
+    async function getAllTabUrls() {
+        let tabs = await chrome.tabs.query({});
+        let urls = tabs.map(t => {return { url: new URL(t.url), tab: t }});
+        return urls;
+    };
+    parser.getAllTabUrls = getAllTabUrls;
+
+
+    function parseByHost(urls) {
+        let hosts = [];
+        urls.forEach(url => {
+            if (!hosts.some(host => host.name === url.url.host)) {
+                let host = {};
+                host.name = url.url.host;
+                if (!!url.tab.favIconUrl) {
+                    host.icon = url.tab.favIconUrl;
+                } else {
+                    host.icon = '../icons/tabex16x16.png';
+                }
+                host.children = urls.filter(curl => curl.url.host === host.name);
+                hosts.push(host);
+            }
+        });
+        return hosts.sort((a,b) => {
+            var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+            var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+
+            // names must be equal
+            return 0;
+        });
+    };
+    parser.parseByHost = parseByHost;
+
+    window.TabEx.parser = parser;
+})(window, chrome);
