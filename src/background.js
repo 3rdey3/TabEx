71235c2659c9b33aaa9f8dcbc541a0ef95ca7de3
Navid Forhad <navid.forhad@ucb.com.bd>
2021-10-10 10:18:48 +0600

    show tab count in badge

--- src/background.js
+++ src/background.js
@@ -6,3 +6,20 @@
 chrome.action.onClicked.addListener((tab) => {
     //TODO toggle dark mode in the tab
 });
+
+function setBadgeText() {
+    chrome.tabs.query({}, (dt) => {
+        chrome.action.setBadgeText(
+            {
+                text: dt.length.toString(),
+            });
+    });
+}
+
+chrome.tabs.onUpdated.addListener(setBadgeText);
+chrome.tabs.onCreated.addListener(setBadgeText);
+chrome.tabs.onActivated.addListener(setBadgeText);
+chrome.tabs.onReplaced.addListener(setBadgeText);
+chrome.tabs.onRemoved.addListener(setBadgeText);
+chrome.windows.onFocusChanged.addListener(setBadgeText);
+
