c32acf755991486524d1ce84f6fcaf021f38f98b
Navid Forhad <navid.forhad@ucb.com.bd>
2021-10-11 13:35:53 +0600

    option page skeleton

---
+++ src/option/components/tabexoptions-component.js
@@ -0,0 +1,24 @@
+﻿(function(window) {
+    let {Vue, Vuex, Fuse} = window;
+
+    let TabExOptions = {
+        data() {
+            return {
+            }
+        },
+        mounted() {
+        },
+        methods: {
+        },
+        render() {
+            return Vue.h('div',
+                'TabEx Options'
+            );
+        }
+    };
+
+    window.TabExOptions.components = window.TabExOptions.components || {};
+
+    window.TabExOptions.components.TabExOptions = TabExOptions;
+})(window);
+
---
+++ src/option/options.js
@@ -0,0 +1,11 @@
+﻿(function (window) {
+    let { Vue, TabExOptions } = window;
+    let { store } = TabExOptions;
+    let app = Vue.createApp(window.TabExOptions.components.TabExOptions);
+    // app.component('leaf-comp', window.TabEx.components.leaf);
+    // app.component('node-comp', window.TabEx.components.node);
+    app.use(store);
+    app.mount('#main');
+    window.TabEx.app = app;
+})(window);
+
